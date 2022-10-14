e3bb0d2634321cd5550d04bdac168878ff252adf
Pabasaraa <pabasara.was@gmail.com>
2022-10-14 16:47:17 +0530

    Initial commit

---
+++ Screens/Home.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { useState } from 'react'
+import { View, SafeAreaView, FlatList } from 'react-native'
+
+//Flatlist - only render items that appear on the screen. And they will get deleted if they are not displayed.
+//it save memory, Take less time to load.
+
+//SafeAreaView - Render content inside the safe area boundry of a device to make sure that everything fits on the screen.
+
+import { COLORS, NFTData } from '../constants'
+import { NFTCard, HomeHeader, FocusedStatusBar } from '../components'
+
+const Home = () => {
+
+  const [nftData, setNftData] = useState(NFTData)
+
+  const handleSearch = (value) => {
+    if(!value.length) return setNftData(NFTData);
+
+    const filteredData = NFTData.filter((item) => 
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if(filteredData.length) {
+      setNftData(filteredData);
+    }else {
+      setNftData(NFTData)
+    }
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+
+      <View style={{ flex: 1 }}>
+        
+        <View style={{ zIndex: 0 }}>
+          <FlatList 
+            data={nftData}
+            renderItem={({ item }) => <NFTCard data={item}/>}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch}/>}
+          />
+        </View>
+
+        <View style={{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          zIndex: -1,
+        }}>
+          <View style={{ height: 300, backgroundColor: COLORS.primary }}/>
+          <View style={{ flex: 1, backgroundColor: COLORS.white }}/>
+        </View>
+
+      </View>
+
+    </SafeAreaView>
+  )
+}
+
+export default Home
